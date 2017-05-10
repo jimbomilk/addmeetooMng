7250701ae4caee4782a556d73224a7e9bc1d703e
Jose Maria Garcia <jmgarciacarrasco@gmail.com>
2017-05-10 21:54:17 +0200

    ok

--- config/database.php
+++ config/database.php
@@ -56,7 +56,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
+            'database' => env('DB_DATABASE', 'addmeetoo'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', 'P4WuoQrlNthgcuMDjqq7'),
             'charset' => 'utf8',
