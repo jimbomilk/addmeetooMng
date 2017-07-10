be62df932386dee7705244f537768108a331aa6f
Jose Maria Garcia <jmgarciacarrasco@gmail.com>
2017-07-10 12:35:05 +0200

    ok

--- resources/views/admin/messages/partials/fields.blade.php
+++ resources/views/admin/messages/partials/fields.blade.php
@@ -3,5 +3,5 @@
 @include("admin.common.input_select",array('var'=>'type','col'=>$types))
 @include("admin.common.input_select",array('var'=>'location_id','col'=>$locations))
 @include("admin.common.input_text",array('var'=>'stext'))
-
+@include("admin.common.input_textarea",array('var'=>'ltext'))
 @include("admin.common.input_file",array('var'=>'image','width'=>360,'height'=>360))
