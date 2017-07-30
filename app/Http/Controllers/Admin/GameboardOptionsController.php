8d2487d438b29b26260d67b877fc2b7d36ae108d
Jose Maria Garcia <jmgarciacarrasco@gmail.com>
2017-07-30 16:48:33 +0500

    ok

--- app/Http/Controllers/Admin/GameboardOptionsController.php
+++ app/Http/Controllers/Admin/GameboardOptionsController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 
 class GameboardOptionsController extends Controller {
 
