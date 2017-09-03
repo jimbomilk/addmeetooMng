550dda8463c76256e474f5d0a984881077dee0b7
Jose Maria Garcia <jmgarciacarrasco@gmail.com>
2017-09-04 00:40:12 +0200

    ok

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -447,7 +447,7 @@ class ApiController extends Controller
 
         $usergameboards = DB::select(DB::raw($query));
 
-        return response()->json($usergameboards,Carbon::now()->month);
+        return response()->json([rank=>$usergameboards,month=>Carbon::now()->format('F')]);
 
     }
 
