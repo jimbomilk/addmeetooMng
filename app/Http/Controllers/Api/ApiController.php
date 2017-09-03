a0079c6f207b8ccdb673c238aec097159672b2a7
Jose Maria Garcia <jmgarciacarrasco@gmail.com>
2017-09-03 19:17:44 +0200

    ok

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -333,15 +333,17 @@ class ApiController extends Controller
                 packs.advertisement_id = ads.id AND
                 packs.smallpack >0 AND
                 packs.latitude BETWEEN (' . $latitude . ' - (packs.radio*0.0117)) AND (' . $latitude . ' + (packs.radio*0.0117)) AND
-                packs.longitude BETWEEN (' . $longitude . ' - (packs.radio*0.0117)) AND (' . $longitude . ' + (packs.radio*0.0117)
-                ORDER BY RAND() LIMIT 10';
+                packs.longitude BETWEEN (' . $longitude . ' - (packs.radio*0.0117)) AND (' . $longitude . ' + (packs.radio*0.0117))
+                ORDER BY RAND()
+                LIMIT 10';
         }
         else{
             $query = 'SELECT * from advertisements ads
                       JOIN adspacks packs ON
                       packs.advertisement_id = ads.id AND
                       packs.smallpack >0
-                      ORDER BY RAND() LIMIT 10';
+                      ORDER BY RAND()
+                      LIMIT 10';
         }
 
 
