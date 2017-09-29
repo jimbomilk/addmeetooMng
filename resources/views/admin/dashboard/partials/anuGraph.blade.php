9a5e311a768c6ee39be456750c03aa8809f43b6b
Jose Maria Garcia <jmgarciacarrasco@gmail.com>
2017-09-29 13:16:51 +0200

    ok

---
+++ resources/views/admin/dashboard/partials/anuGraph.blade.php
@@ -0,0 +1,12 @@
+<!-- Custom tabs (Charts with tabs)-->
+<div class="nav-tabs-custom">
+    <!-- Tabs within a box -->
+    <ul class="nav nav-tabs pull-right">
+        <li class="pull-left header"><i class="fa fa-inbox"></i> Mis anuncios</li>
+    </ul>
+    <div class="tab-content no-padding">
+        <!-- Morris chart - Sales -->
+        <div class="chart tab-pane active" id="revenue-chart" style="position: relative; height: 300px;"></div>
+        <div class="chart tab-pane" id="sales-chart" style="position: relative; height: 300px;"></div>
+    </div>
+</div><!-- /.nav-tabs-custom -->
