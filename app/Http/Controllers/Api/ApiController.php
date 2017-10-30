2eb752a52c066b6d64c6a8e176cd22d8268069f0
Jose Maria Garcia <jmgarciacarrasco@gmail.com>
2017-10-30 13:33:25 +0100

    ok

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -407,18 +407,16 @@ class ApiController extends Controller
              $query = "SELECT ads.*,packs.id as packid  from advertisements ads
                 JOIN adspacks packs ON
                 packs.advertisement_id = ads.id AND
-                packs.startdate <='". $now . "' AND packs.enddate >'". $now . "' AND
                 packs.latitude BETWEEN (" . $latitude . " - (packs.radio*0.0117)) AND (" . $latitude . " + (packs.radio*0.0117)) AND
                 packs.longitude BETWEEN (" . $longitude . " - (packs.radio*0.0117)) AND (" . $longitude . " + (packs.radio*0.0117))
-                WHERE ads.location_id = ". $location ."
+                WHERE ads.location_id = ". $location ." AND packs.startdate <='". $now . "' AND packs.enddate >'". $now . "'
                 ORDER BY CASE ads.id WHEN 16 THEN -1 ELSE RAND() END LIMIT 20";
         }
         else{
              $query = "SELECT ads.*,packs.id as packid from advertisements ads
                       JOIN adspacks packs ON
-                      packs.advertisement_id = ads.id AND
-                      packs.startdate <='". $now . "' AND packs.enddate >'". $now . "'
-                      WHERE ads.location_id = ". $location. "
+                      packs.advertisement_id = ads.id
+                      WHERE ads.location_id = ". $location. " AND packs.startdate <='". $now . "' AND packs.enddate >'". $now . "'
                       ORDER BY CASE ads.id WHEN 16 THEN -1 ELSE RAND() END LIMIT 20";
         }
 
