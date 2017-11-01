66d9456acacffe90b4788fedc7b3b216795f07d2
Jose Maria Garcia <jmgarciacarrasco@gmail.com>
2017-11-01 13:59:50 +0100

    ok

---
+++ resources/views/admin/dashboard/partials/messages.blade.php
@@ -0,0 +1,26 @@
+<!-- TO DO List -->
+<div class="box box-primary">
+    <div class="box-header">
+        <i class="ion ion-clipboard"></i>
+        <h3 class="box-title">Mensajes</h3>
+
+    </div><!-- /.box-header -->
+    <div class="box-body">
+        <ul class="todo-list">
+            @foreach($messages as $msg)
+            <li>
+                <img src="{{$msg->image}}" width="75" height="50" alt="add image" class="online"/>
+                <!-- todo text -->
+                <span class="text">{{$msg->stext}}</span>
+                <span class="text">{{$msg->ltext}}</span>
+
+            </li>
+            @endforeach
+            <span class="box-tools pull-right inline">
+            {{ $messages->links() }}
+            </span>
+
+        </ul>
+    </div><!-- /.box-body -->
+
+</div><!-- /.box -->
---
+++ resources/views/admin/gameboards/participants.blade.php
@@ -0,0 +1,6 @@
+@section('scripts')
+    <script>
+        <google-chart [data]="chartData" style="width: 100%"></google-chart>
+    </script>
+
+@endsection
