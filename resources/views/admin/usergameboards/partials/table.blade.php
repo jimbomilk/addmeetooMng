db0551d0ea625eed462adaf3108502159d348274
Jose Maria Garcia <jmgarciacarrasco@gmail.com>
2017-11-02 19:07:19 +0100

    ok

--- resources/views/admin/usergameboards/partials/table.blade.php
+++ resources/views/admin/usergameboards/partials/table.blade.php
@@ -2,6 +2,7 @@
     <tr>
         <th>Rank</th>
         <th>Name</th>
+        <th>Id</th>
         <th>Points</th>
         @if ($login_user->is('admin'))
         <th></th>
@@ -11,6 +12,7 @@
         <tr data-id="{{$usergameboard->id}}">
             <td>{{(($set->currentPage()-1) * $set->perPage()) + $indexKey+1}}</td>
             <td>{{$usergameboard->user->name}}</td>
+            <td>{{$usergameboard->user->id}}</td>
             <td>{{$usergameboard->points}}</td>
             @if ($login_user->is('admin'))
             <td>
