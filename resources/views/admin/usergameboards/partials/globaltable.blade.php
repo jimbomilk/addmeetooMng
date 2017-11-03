39d63b01912ff92d9f150bdc16c05dd30ed567ac
Jose Maria Garcia <jmgarciacarrasco@gmail.com>
2017-11-03 08:12:25 +0100

    ok

---
+++ resources/views/admin/usergameboards/partials/globaltable.blade.php
@@ -0,0 +1,27 @@
+<table class="table table-striped">
+    <tr>
+        <th>Rank</th>
+        <th>Nombre</th>
+        <th>Id</th>
+        <th>Puntos</th>
+        @if ($login_user->is('admin'))
+        <th></th>
+        @endif
+    </tr>
+    @foreach($set as $indexKey => $usergameboard)
+        <tr data-id="{{$usergameboard->id}}">
+            <td>{{(($set->currentPage()-1) * $set->perPage()) + $indexKey+1}}</td>
+            <td>{{$usergameboard->user->name}}</td>
+            <td>{{$usergameboard->user->id}}</td>
+            <td>{{$usergameboard->points}}</td>
+            @if ($login_user->is('admin'))
+            <td>
+                @include("admin.common.btn_edit",array('var'=>$usergameboard))
+                @include("admin.common.btn_delete",array('var'=>$usergameboard))
+            </td>
+            @endif
+        </tr>
+    @endforeach
+    <div class="pagination"> {{ $set->appends(request()->input())->links() }} </div>
+
+</table>
---
+++ resources/views/admin/usergameboards/partials/monthlytable.blade.php
@@ -0,0 +1,19 @@
+<table class="table table-striped">
+    <tr>
+        <th>Rank</th>
+        <th>Nombre</th>
+        <th>Email</th>
+        <th>Puntos</th>
+
+    </tr>
+    @foreach($set as $indexKey => $userrank)
+        <tr data-id="{{$userrank->id}}">
+            <td>{{$indexKey+1}}</td>
+            <td>{{$userrank->name}}</td>
+            <td>{{$userrank->email}}</td>
+            <td>{{$usergameboard->points}}</td>
+
+        </tr>
+    @endforeach
+
+</table>
