78f742834c78af9a45336c8430e02aa33fc2e180
Jose Maria Garcia <jmgarciacarrasco@gmail.com>
2017-11-04 13:30:31 +0100

    ok

--- resources/views/admin/gameboards/participants.blade.php
+++ resources/views/admin/gameboards/participants.blade.php
@@ -7,24 +7,25 @@
     <div class="container" >
         <div class="row">
             <div class="col-md-12" style="padding-right: 60px">
+
+
+                <!-- Custom tabs (Charts with tabs)-->
                 <div class="panel panel-default">
-
                     <!-- Custom tabs (Charts with tabs)-->
-                    <div class="panel panel-default">
-                        <!-- Custom tabs (Charts with tabs)-->
-                        <div class="nav-tabs-custom">
-                            <!-- Tabs within a box -->
-                            <ul class="nav nav-tabs pull-right">
-                                <li class="pull-left header"><i class="fa fa-inbox"></i> Resultados</li>
-                            </ul>
-                            <div class="tab-content no-padding">
-                                <!-- Morris chart - Sales -->
-                                <div class="chart tab-pane active" id="results-chart" style="position: relative; height: 300px;width: 300px;margin: auto"></div>
+                    <div class="nav-tabs-custom">
+                        <!-- Tabs within a box -->
+                        <ul class="nav nav-tabs pull-right">
+                            <li class="pull-left header"><i class="fa fa-inbox"></i> Resultados</li>
+                        </ul>
+                        <div class="tab-content no-padding">
+                            <!-- Morris chart - Sales -->
+                            <div class="chart tab-pane active" id="results-chart" style="position: relative; height: 300px;width: 300px;margin: auto"></div>
 
-                            </div>
-                        </div><!-- /.nav-tabs-custom -->
+                        </div>
+                    </div><!-- /.nav-tabs-custom -->
 
-                    </div>
+                </div>
+                <div class="panel panel-default">
                     <div class="nav-tabs-custom">
                         <!-- Tabs within a box -->
                         <ul class="nav nav-tabs pull-right">
